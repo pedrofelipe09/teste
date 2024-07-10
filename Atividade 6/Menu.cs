1f2c3c90b9bce9fc75e2f06bfb0e9ef53794d101
duda-1 <163910040+pedrofelipe09@users.noreply.github.com>
2024-07-10 17:17:06 -0300

    Menu

---
+++ Atividade 6/Menu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_6
+{
+    public class Menu
+    {
+        public int num1 { get; set; } 
+        public int num2 { get; set; }
+        public int num3 { get; set; }
+
+        public void ExibirMenu()
+        {
+            Console.WriteLine("Digite um número");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("1- Adição \n2- Subtração \n3- Multiplicação \n4- Divisao");
+            int acao = int.Parse(Console.ReadLine());
+            if(acao == 1)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(num3);
+            }
+            else if (acao == 2)
+            {
+                num3= num1  - num2;
+                Console.WriteLine(num3);    
+            }
+            else if (acao == 3)
+            {
+                num3 = num1 * num2;
+                Console.WriteLine(num3);
+            }
+            else if (acao == 4)
+            {
+                num3 = num1 / num2;
+                Console.WriteLine(num3);
+            }
+        }
+
+    }
+}
